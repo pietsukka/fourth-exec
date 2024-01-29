1568b6f19bfb91e671ab65ffb36f257a873c79c7
Juho-Ville <ejuui001@edu.xamk.fi>
2024-01-29 14:52:37 +0200

    Made the error page look nicer

--- react-vite/src/main.jsx
+++ react-vite/src/main.jsx
@@ -32,30 +32,42 @@ import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Root />,
-    errorElement: <ErrorPage />,
-    loader: rootLoader,
-    action: rootAction,
-    children: [
-      { index: true, element: <Index /> },
-      {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-        action: contactAction,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-        errorElement: <div>Oops! There was an error.</div>,
-      },
+        path: "/",
+        element: <Root />,
+        errorElement: <ErrorPage />,
+        loader: rootLoader,
+        action: rootAction,
+        children: [
+          {
+            errorElement: <ErrorPage />,
+            children: [
+              { index: true, element: <Index /> },
+              {
+                path: "contacts/:contactId",
+                element: <Contact />,
+                loader: contactLoader,
+                action: contactAction,
+              },
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
+      },    
     ],
   },
 ]);
