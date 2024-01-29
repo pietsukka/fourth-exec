6156715d9fa4cd6fac7bc73121994bdd643b4099
Juho-Ville <ejuui001@edu.xamk.fi>
2024-01-29 11:33:20 +0200

    started using loader

--- react-vite/src/routes/root.jsx
+++ react-vite/src/routes/root.jsx
@@ -1,9 +1,20 @@
-import { Outlet, Link } from "react-router-dom";
+import {
+    Outlet,
+    Link,
+    useLoaderData,
+  } from "react-router-dom";
+  import { getContacts } from "../contacts";
 
+// exports loader
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+  }
 
 //Exprots the root
 
 export default function Root() {
+    const { contacts } = useLoaderData();
     return (
       <>
         <div id="sidebar">
@@ -32,14 +43,28 @@ export default function Root() {
             </form>
           </div>
           <nav>
+          {contacts.length ? (
             <ul>
-              <li>
-                <Link to={`/contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                <Link to={`/contacts/2`}>Your Friend</Link>
-              </li>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
             </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
           </nav>
         </div>
         <div id="detail">
